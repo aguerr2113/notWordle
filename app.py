997cff1d962d7c13e62ae81c670ae5e920221ad1
aguerrero2113 <aguerrero2113@github.com>
2023-02-12 21:42:57 -0600

    created a simple wordle clone with a a full list of 5 letter words.

---
+++ app.py
@@ -0,0 +1,34 @@
+
+import os, random
+os.system("clear")
+from words import words
+
+BG_GREEN = "\u001b[42m"
+BG_YELLOW = "\u001b[43m"
+RESET = "\u001b[0m"
+
+print("WORDLE")
+
+correct = random.choice(words)
+# You get six chances to guess
+for _ in range(6):
+    guess = input("Please guess. > ").upper()
+
+    # Check each letter
+    for i in range(0, 5):
+        if guess[i]==correct[i]:
+            print(f"{BG_GREEN}{guess[i]}{RESET}", end="")
+        elif guess[i] in correct:
+            print(f"{BG_YELLOW}{guess[i]}{RESET}", end="")
+        else:
+            print(guess[i], end="")
+            
+    print()
+
+    # Check if the guess is correct
+    if guess == correct:
+        print("You win!")
+        exit()
+        
+print("You lose!")
+print(f"The correct word was {correct}.")
